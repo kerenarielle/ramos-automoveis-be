27fee750986831e6146d97ccbf07eb1707eaf716
Keren <keren.ariellemr@gmail.com>
2023-07-03 09:21:33 -0300

    fix

--- server/index.js
+++ server/index.js
@@ -24,7 +24,6 @@ const isProduction = process.env.NODE_ENV === "production";
 // Middleware de CORS
 app.use(
   cors({
-    "Access-Control-Allow-Origin": "*",
     origin: process.env.APP_CORS,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     credentials: true,
